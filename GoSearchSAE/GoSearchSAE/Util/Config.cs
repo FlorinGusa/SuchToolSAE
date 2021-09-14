719dc9d0a937d17f9aed4b8726f1db61db8928bb
nils-haesicke <85865746+nils-haesicke@users.noreply.github.com>
2021-09-14 09:31:10 +0200

    Config reader

---
+++ GoSearchSAE/GoSearchSAE/Util/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO
+
+namespace GoSearchSAE
+{
+    public class Config
+    {
+        private void config(object sender, EventArgs e)
+        {
+            FileStream config_data = new FileStream(@"config\\config.txt", FileMode.Open);
+            StreamReader config_reader = new StreamReader(config_data);
+            string config_text = config_reader.ReadToEnd();
+            config_reader.Close();
+            config_data.Close();
+        }
+    }
+}
