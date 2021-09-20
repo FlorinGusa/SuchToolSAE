d0158102ffda9fb96585dc7fa3200aef098e8323
Florin Gusa <florin.gusa@fanuc.eu>
2021-09-20 08:12:55 +0200

    commands

---
+++ GoSearchSAE/GoSearchSAE/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GoSearchSAE
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        Action<object> ExecuteAction;
+        Predicate<object> CanExecutePredicate;
+
+        public Command(Action<object> executeAction, Predicate<object> predicateAction = null)
+        {
+            ExecuteAction = executeAction;
+            CanExecutePredicate = predicateAction;
+        }
+
+
+        public bool CanExecute(object parameter) => CanExecutePredicate == null || CanExecutePredicate(parameter);
+        
+        public void Execute(object parameter)
+        {
+            ExecuteAction(parameter);
+        }
+
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
--- GoSearchSAE/GoSearchSAE/MainViewModel.cs
+++ GoSearchSAE/GoSearchSAE/MainViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using GoSearchSAE.Util;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 namespace GoSearchSAE
 {
     class MainViewModel : PropertyNotifier
     {
+        public Command ShowInExplorerCommand { get; }
+
+
+        public MainViewModel()
+        {
+            ShowInExplorerCommand = new Command(ShowInExplorer);
+        }
+
         private ListItem _SelectedItem;
         public ListItem SelectedItem
         {
@@ -17,6 +27,38 @@ namespace GoSearchSAE
             }
         }
         private List<ListItem> _items;
+        public List<ListItem> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        private string _SearchText;
+
+        public string SearchText
+        {
+            get => _SearchText;
+            set
+            {
+                _SearchText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void ShowInExplorer(object param)
+        {
+            if (SelectedItem == null)
+                return;
+           
+            string path = Path.Combine(SelectedItem.Path, SelectedItem.Name);
+            Process.Start("explorer.exe", "/n, /select, \"" + path + "\"");
+        }
+
 
     }
 }
